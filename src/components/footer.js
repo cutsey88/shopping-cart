42a32ba59b14bdd73dc42e3f9d977fd187bb1aa0
Chris Utsey <chrisautsey88@gmail.com>
2022-11-26 16:40:29 +0900

    Reformat footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,17 @@
 function Footer() {
     return (
         <nav className="footer">
-            <div className="logo">
-                <p>Counter Parts</p>
-            </div>
             <div className="footer-links">
                 <div className="f-bucket-1">
+                    <p>About Us</p>
                     <p>Contact</p>
-                    <p>About</p>
                 </div>
                 <div className="f-bucket-2">
-                    <p>Store</p>
+                    <p>Shipping</p>
                     <p>FAQ</p>
                 </div>
             </div>
+            <small>Copyright &copy; 2022 Christopher A. Utsey</small>
         </nav>
     )
 }
