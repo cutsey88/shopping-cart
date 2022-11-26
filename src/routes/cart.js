21e8a60622bd80a272a45015c51925402c2857b9
Chris Utsey <chrisautsey88@gmail.com>
2022-11-27 02:28:57 +0900

    Format for mobile

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,10 +1,24 @@
 import "../styles/cart.css";
-import { useOutletContext } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 import CartItem from "../components/cartItem";
 
 function Cart() {
 
     const cartObj = useOutletContext();
+
+    if (cartObj.cart.products.length === 0) {
+        return (
+            <div className="cart empty-cart">
+                <div className="cart-header empty-cart-header" >
+                    <p>Cart</p>
+                </div>
+                <div className="empty-cart-message">
+                    <p>Your cart is empty.</p>
+                    <Link className="empty-cart-link" to="/shop/all">Shop Now</Link>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className="cart">
